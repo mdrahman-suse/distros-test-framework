1275d506d75e521ef6ba278c082f78282ab8d7e3
mdrahman-suse <mdrahman@suse.com>
2024-08-29 10:47:03 -0500

    cleanup code

--- scripts/delete_resources.sh
+++ scripts/delete_resources.sh
@@ -20,12 +20,11 @@ if [[ ! -f ./config/"$PRODUCT_NAME".tfvars ]]; then
   exit 1
 fi
 
-printf "This is going to delete all AWS resources with the prefix %s. Continue (yes/no)? " "$RESOURCE_NAME"
-read -r REPLY
-if [[ "$REPLY" =~ ^[Yy][Ee][Ss]$ ]]; then
+read -p "Delete all AWS resources with the prefix $RESOURCE_NAME? [y/n]: " -n 1 -r
+if [[ $REPLY =~ ^[Yyes]$ ]]; then
   NAME_PREFIX="$RESOURCE_NAME"
 
-  echo "Terminating resources for $NAME_PREFIX if still up and running"
+  echo -e "\nTerminating resources for $NAME_PREFIX if running"
   # shellcheck disable=SC2046
   aws ec2 terminate-instances --instance-ids $(aws ec2 describe-instances \
     --filters "Name=tag:Name,Values=${NAME_PREFIX}*" \
@@ -39,7 +38,6 @@ if [[ "$REPLY" =~ ^[Yy][Ee][Ss]$ ]]; then
     aws rds delete-db-instance --db-instance-identifier "$instance" --skip-final-snapshot > /dev/null 2>&1
   done
 
-
   #Search for DB clusters and delete them
   CLUSTERS=$(aws rds describe-db-clusters --query "DBClusters[?starts_with(DBClusterIdentifier,
    '${NAME_PREFIX}')].DBClusterIdentifier" --output text 2> /dev/null)
@@ -48,7 +46,6 @@ if [[ "$REPLY" =~ ^[Yy][Ee][Ss]$ ]]; then
     aws rds wait db-cluster-deleted --db-cluster-identifier "$cluster"
   done
 
-
   #Search for DB snapshots and delete them
   SNAPSHOTS=$(aws rds describe-db-snapshots --query "DBSnapshots[?starts_with(DBSnapshotIdentifier,
    '${NAME_PREFIX}')].DBSnapshotIdentifier" --output text 2> /dev/null)
@@ -56,12 +53,10 @@ if [[ "$REPLY" =~ ^[Yy][Ee][Ss]$ ]]; then
     aws rds delete-db-snapshot --db-snapshot-identifier "$snapshot" > /dev/null 2>&1
   done
 
-
   #Get the list of load balancer ARNs
   LB_ARN_LIST=$(aws elbv2 describe-load-balancers \
     --query "LoadBalancers[?starts_with(LoadBalancerName, '${NAME_PREFIX}') && Type=='network'].LoadBalancerArn" \
     --output text)
-
 
   #Loop through the load balancer ARNs and delete the load balancers
   for LB_ARN in $LB_ARN_LIST; do
@@ -74,13 +69,11 @@ if [[ "$REPLY" =~ ^[Yy][Ee][Ss]$ ]]; then
     --query "TargetGroups[?starts_with(TargetGroupName, '${NAME_PREFIX}') && Protocol=='TCP'].TargetGroupArn" \
     --output text)
 
-
   #Loop through the target group ARNs and delete the target groups
   for TG_ARN in $TG_ARN_LIST; do
     echo "Deleting target group $TG_ARN"
     aws elbv2 delete-target-group --target-group-arn "$TG_ARN"
   done
-
 
   #Get the ID and recordName with lower case of the hosted zone that contains the Route 53 record sets
   NAME_PREFIX_LOWER=$(echo "$NAME_PREFIX" | tr '[:upper:]' '[:lower:]')
@@ -91,13 +84,11 @@ if [[ "$REPLY" =~ ^[Yy][Ee][Ss]$ ]]; then
     --query "ResourceRecordSets[?starts_with(Name, '${NAME_PREFIX_LOWER}.') && Type == 'CNAME'].Name" \
     --output text)
 
-
   #Get ResourceRecord Value
   RECORD_VALUE=$(aws route53 list-resource-record-sets \
     --hosted-zone-id "${R53_ZONE_ID}" \
     --query "ResourceRecordSets[?starts_with(Name, '${NAME_PREFIX_LOWER}.') \
       && Type == 'CNAME'].ResourceRecords[0].Value" --output text)
-
 
   #Delete Route53 record
   if [[ "$R53_RECORD" == "${NAME_PREFIX_LOWER}."* ]]; then
